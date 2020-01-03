980f22e09df98cacf98d57ee881f4b586ba18433
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2020-01-03 22:03:50 +0100

    Add additional input shape

--- keras_segmentation/models/_pspnet_2.py
+++ keras_segmentation/models/_pspnet_2.py
@@ -217,6 +217,11 @@ def interp_block(prev_layer, level, feature_map_shape, input_shape):
                               2: 15,
                               3: 10,
                               6: 5}
+    elif input_shape == (285, 1252):
+        kernel_strides_map = {1: 30,
+                              2: 20,
+                              3: 15,
+                              6: 10}
     elif input_shape == (380, 1670):
         kernel_strides_map = {1: 40,
                               2: 30,
